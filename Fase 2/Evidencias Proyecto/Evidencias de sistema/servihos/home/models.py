8f91e4627003b81964ec94116a3e012b995c87b0
brunoduoc <brun.uribe@duocuc.cl>
2025-05-11 17:51:19 -0400

    initial database models

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/models.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class usuarioCustom(AbstractUser):
+    rut = models.IntegerField (blank=False, unique=True)
+
+    REQUIRED_FIELDS = ['rut']
+
+    TIPO_CHOICES = [
+        ('administrador', 'Administrador'),
+        ('encargado', 'Encargado'),
+    ]
+
+    tipo = models. CharField(
+        max_length=25,
+        choices=TIPO_CHOICES,
+        default='encargado',
+        blank= True,
+        verbose_name="Tipo de usuario"
+    )
+
+    def es_administrador(self):
+        return self.tipo == 'administrador'
+
+    def es_encargado(self):
+        return self.tipo == 'encargado'
+    pass
