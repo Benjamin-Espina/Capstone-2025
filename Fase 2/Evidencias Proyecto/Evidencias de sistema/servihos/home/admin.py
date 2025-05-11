f868b679e6af1f3f73a383e525dc6451285d6768
HappySama <benj.espina@duocuc.cl>
2025-05-11 18:24:51 -0400

    Admin y forms

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/admin.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import usuarioCustom
+from .forms import customUserCreationForm
 
-# Register your models here.
+class UsuarioCustomAdmin(UserAdmin):
+    add_form = customUserCreationForm  # <--- Esta línea es clave
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'rut', 'tipo', 'is_staff', 'is_superuser', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información Personal', {'fields': ('first_name', 'last_name', 'email', 'rut', 'tipo')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'rut', 'tipo', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'first_name', 'last_name', 'rut')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+admin.site.register(usuarioCustom, UsuarioCustomAdmin)
---
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import usuarioCustom
+from django.contrib.auth.forms import UserCreationForm
+
+class customUserCreationForm(UserCreationForm):
+    rut = forms.IntegerField(
+        label='Rut',
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    first_name = forms.CharField(
+        label='Nombre',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        label='Apellido',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Nombre de usuario'
+        self.fields['password1'].label = 'Contraseña'
+        self.fields['password2'].label = 'Confirmar contraseña'
+
+    class Meta:
+        model = usuarioCustom
+        fields = ('username', 'first_name', 'last_name', 'rut', 'password1', 'password2')
+        labels = {
+            'username': 'Nombre de usuario',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar contraseña',
+        }
