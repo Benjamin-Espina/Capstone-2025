2e690326f50ae59aa54475b18522f24e92b52f3b
brunoduoc <brun.uribe@duocuc.cl>
2025-05-13 19:28:40 -0400

    creación de tabla usuario hospederia

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/models.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/servihos/home/models.py
@@ -25,3 +25,37 @@ class usuarioCustom(AbstractUser):
     def es_encargado(self):
         return self.tipo == 'encargado'
     pass
+
+class tipo_discapacidad(models.Model):
+    id_tipo_discapacidad = models.AutoField(primary_key=True)
+    descripcion_tipo_discapacidad = models.CharField(max_length=150, blank=False)
+
+    def __str__(self):
+        return self.descripcion_tipo_discapacidad
+    
+class hospederia(models.Model):
+    id_hospederia = models.AutoField(primary_key=True)
+    nombre_hospederia = models.CharField(max_length=50, blank=False)
+    direccion_hospederia = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.nombre_hospederia    
+
+class usuario_hospederia(models.Model):
+    rut_usr_hospederia = models.IntegerField(primary_key=True,blank=False, unique=True)
+    pasaporte_usr_hospederia = models.CharField(max_length=25, blank=False)
+    primer_nombre_usr_hospederia = models.CharField(max_length=25, blank=False)
+    segundo_nombre_usr_hospederia = models.CharField(max_length=25, blank=False)
+    primer_apellido_usr_hospederia = models.CharField(max_length=25, blank=False)
+    segundo_apellido_usr_hospederia = models.CharField(max_length=25, blank=False)
+    fecha_nacimiento_usr_hospederia = models.DateField(blank=False)
+    discapacidad_usr_hospederia = models.BooleanField(blank=False)
+    id_tipo_discapacidad = models.ForeignKey(tipo_discapacidad, on_delete=models.CASCADE, blank=True, null=True)
+    nacionalidad_usr_hospederia = models.CharField(max_length=25, blank=False)
+    id_hospederia = models.ForeignKey(hospederia, on_delete=models.CASCADE, blank=True, null=True)
+    mostrar_en_reportes = models.BooleanField(blank=False)
+
+    def __str__(self):
+        return self.primer_nombre_usr_hospederia
+
+
