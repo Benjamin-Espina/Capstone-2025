4d32b3ffec9e71e5ce60e184c498be76eb59ba4a
brunoduoc <brun.uribe@duocuc.cl>
2025-07-08 18:32:46 -0400

    .

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Base de datos/bd servihos.sql
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Base de datos/bd servihos.sql
@@ -1,112 +1,112 @@
-CREATE TABLE `servicio` (
-  `id_servicio` int PRIMARY KEY,
-  `nombre_servicio` varchar(100),
-  `observacion` varchar(500)
-);
+Table servicio {
+  id_servicio int [pk]
+  nombre_servicio varchar(100)
+  observacion varchar(500)
+}
 
-CREATE TABLE `subservicio` (
-  `id_subservicio` int PRIMARY KEY,
-  `id_servicio` int,
-  `nombre_subservicio` varchar(100)
-);
+Table subservicio {
+  id_subservicio int [pk]
+  id_servicio int
+  nombre_subservicio varchar(100)
+}
 
-CREATE TABLE `tipo_discapacidad` (
-  `id_discapacidad` int PRIMARY KEY,
-  `tipo_discapacidad` varchar(100),
-  `descripcion_discapacidad` varchar(200)
-);
+Table tipo_discapacidad {
+  id_discapacidad int [pk]
+  tipo_discapacidad varchar(100)
+  descripcion_discapacidad varchar(200)
+}
 
-CREATE TABLE `comuna` (
-  `id_comuna` int PRIMARY KEY,
-  `nombre_comuna` varchar(30)
-);
+Table comuna {
+  id_comuna int [pk]
+  nombre_comuna varchar(30)
+}
 
-CREATE TABLE `hospederia` (
-  `id_hospederia` int PRIMARY KEY,
-  `nombre_hospederia` varchar(100),
-  `direccion_hospederia` varchar(100),
-  `id_comuna` int
-);
+Table hospederia {
+  id_hospederia int [pk]
+  nombre_hospederia varchar(100)
+  direccion_hospederia varchar(100)
+  id_comuna int
+}
 
-CREATE TABLE `usuario_hospederia` (
-  `rut_usuario_hospederia` int PRIMARY KEY,
-  `pasaporte_usuario_hospederia` varchar(9),
-  `primer_nombre_usuario_hospederia` varchar(20),
-  `segundo_nombre_usuario_hospederia` varchar(20),
-  `primer_apellido_usuario_hospederia` varchar(20),
-  `segundo_apellido_usuario_hospederia` varchar(20),
-  `fecha_nacimiento` timestamp,
-  `discapacidad` boolean,
-  `id_tipo_discapacidad` int,
-  `nacionalidad` varchar(30),
-  `id_hospederia` int,
-  `mostrar_en_reportes` boolean
-);
+Table usuario_hospederia {
+  rut_usuario_hospederia int [pk]
+  pasaporte_usuario_hospederia varchar(9)
+  primer_nombre_usuario_hospederia varchar(20)
+  segundo_nombre_usuario_hospederia varchar(20)
+  primer_apellido_usuario_hospederia varchar(20)
+  segundo_apellido_usuario_hospederia varchar(20)
+  fecha_nacimiento timestamp
+  discapacidad boolean
+  id_tipo_discapacidad int
+  nacionalidad varchar(30)
+  id_hospederia int
+  mostrar_en_reportes boolean
+}
 
-CREATE TABLE `usuario_encargado` (
-  `rut_usuario_encargado` int PRIMARY KEY,
-  `primer_nombre_usuario_encargado` varchar(20),
-  `primer_apellido_usuario_encargado` varchar(20),
-  `usuario` varchar(20),
-  `contrasena` varchar(60),
-  `id_hospederia` int
-);
+Table usuario_encargado {
+  id_encargado int [pk, increment]
+  rut_usuario_encargado int [unique]
+  primer_nombre_usuario_encargado varchar(20)
+  primer_apellido_usuario_encargado varchar(20)
+  usuario varchar(20)
+  contrasena varchar(60)
+  id_hospederia int
+  id_cargo int
+}
 
-CREATE TABLE `administrador` (
-  `id_admin` int PRIMARY KEY,
-  `primer_nombre_admin` varchar(20),
-  `primer_apellido_admin` varchar(20),
-  `segundo_apellido_admin` varchar(20),
-  `usuario` varchar(20),
-  `contrasena` varchar(60)
-);
+Table administrador {
+  id_admin int [pk]
+  primer_nombre_admin varchar(20)
+  primer_apellido_admin varchar(20)
+  segundo_apellido_admin varchar(20)
+  usuario varchar(20)
+  contrasena varchar(60)
+  id_cargo int
+}
 
-CREATE TABLE `cargo` (
-  `id_cargo` int PRIMARY KEY,
-  `nombre_cargo` varchar(50),
-  `descripcion_cargo` varchar(200)
-);
+Table cargo {
+  id_cargo int [pk]
+  nombre_cargo varchar(50)
+  descripcion_cargo varchar(200)
+}
 
-CREATE TABLE `registros` (
-  `id_registro` int PRIMARY KEY,
-  `hora_ingreso` timestamp,
-  `hora_salida` timestamp,
-  `rut_usuario_hospederia` int,
-  `id_reporte` int
-);
+Table registros {
+  id_registro int [pk]
+  hora_ingreso timestamp
+  hora_salida timestamp
+  rut_usuario_hospederia int
+  id_reporte int
+  id_encargado int
+}
 
-CREATE TABLE `reportes` (
-  `id_reporte` int PRIMARY KEY,
-  `tipo_reporte` int,
-  `fecha_reporte` timestamp,
-  `rut_usuario_encargado` int,
-  `id_admin` int
-);
+Table reportes {
+  id_reporte int [pk]
+  tipo_reporte int
+  fecha_reporte timestamp
+  id_encargado int
+  id_admin int
+}
 
-CREATE TABLE `registro_servicio` (
-  `id_registro` int,
-  `id_servicio` int,
-  PRIMARY KEY (`id_registro`, `id_servicio`)
-);
+Table registro_servicio {
+  id_registro int
+  id_servicio int
+  Indexes {
+    (id_registro, id_servicio) [pk]
+  }
+}
 
-ALTER TABLE `subservicio` ADD FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_servicio`);
-
-ALTER TABLE `usuario_hospederia` ADD FOREIGN KEY (`id_tipo_discapacidad`) REFERENCES `tipo_discapacidad` (`id_discapacidad`);
-
-ALTER TABLE `usuario_hospederia` ADD FOREIGN KEY (`id_hospederia`) REFERENCES `hospederia` (`id_hospederia`);
-
-ALTER TABLE `hospederia` ADD FOREIGN KEY (`id_comuna`) REFERENCES `comuna` (`id_comuna`);
-
-ALTER TABLE `usuario_encargado` ADD FOREIGN KEY (`id_hospederia`) REFERENCES `hospederia` (`id_hospederia`);
-
-ALTER TABLE `registros` ADD FOREIGN KEY (`rut_usuario_hospederia`) REFERENCES `usuario_hospederia` (`rut_usuario_hospederia`);
-
-ALTER TABLE `registros` ADD FOREIGN KEY (`id_reporte`) REFERENCES `reportes` (`id_reporte`);
-
-ALTER TABLE `reportes` ADD FOREIGN KEY (`rut_usuario_encargado`) REFERENCES `usuario_encargado` (`rut_usuario_encargado`);
-
-ALTER TABLE `reportes` ADD FOREIGN KEY (`id_admin`) REFERENCES `administrador` (`id_admin`);
-
-ALTER TABLE `registro_servicio` ADD FOREIGN KEY (`id_registro`) REFERENCES `registros` (`id_registro`);
-
-ALTER TABLE `registro_servicio` ADD FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_servicio`);
+// Relaciones
+Ref: subservicio.id_servicio > servicio.id_servicio
+Ref: usuario_hospederia.id_tipo_discapacidad > tipo_discapacidad.id_discapacidad
+Ref: usuario_hospederia.id_hospederia > hospederia.id_hospederia
+Ref: hospederia.id_comuna > comuna.id_comuna
+Ref: usuario_encargado.id_hospederia > hospederia.id_hospederia
+Ref: usuario_encargado.id_cargo > cargo.id_cargo
+Ref: administrador.id_cargo > cargo.id_cargo
+Ref: registros.rut_usuario_hospederia > usuario_hospederia.rut_usuario_hospederia
+Ref: registros.id_reporte > reportes.id_reporte
+Ref: registros.id_encargado > usuario_encargado.id_encargado
+Ref: reportes.id_encargado > usuario_encargado.id_encargado
+Ref: reportes.id_admin > administrador.id_admin
+Ref: registro_servicio.id_registro > registros.id_registro
+Ref: registro_servicio.id_servicio > servicio.id_servicio
